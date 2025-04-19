c596d1afa95f5b507deeb37f87a309fa83c686b3
quangnam <quangnam@gmail.com>
2025-04-19 23:39:17 +0700

    add source code 19/4

---
+++ Source code/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginRegister from './components/LoginRegister';
+import "./assets/LoginRegister.css";
+
+
+
+function App() {
+  return <LoginRegister />;
+}
+
+export default App;
